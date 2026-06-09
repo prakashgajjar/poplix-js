f77be3ebdb53105d2c9637913c7ba3d71d080272
prakash <prakashgajjar096@gmail.com>
2026-06-09 16:47:12 +0530

    logs added for sendpost api

--- app/api/home/post/sendpost/route.js
+++ app/api/home/post/sendpost/route.js
@@ -1,23 +1,27 @@
+export const runtime = "nodejs";
 import cloudinary from "../../../../../lib/cloudinary";
 import { NextResponse } from "next/server";
 import Post from "../../../../../models/Post.models";
 import User from "../../../../../models/User.models";
 import { getUserIdFromToken } from "../../../../../lib/getUserIdfromToken";
 import connectDB from "../../../../../lib/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export async function POST(req) {
   try {
-    await connectDB();
     const formData = await req.formData();
     const file = formData.get("post");
     const content = formData.get("content");
+    await connectDB();
 
     // console.log(formData);
 
     if (!file && !content) {
       return NextResponse.json(
         { error: "No content or file uploaded" },
-        { status: 400 }
+        { status: 400 },
       );
     }
 
@@ -41,7 +45,7 @@ export async function POST(req) {
               (err, result) => {
                 if (err) return reject(new Error("Cloudinary Upload Failed"));
                 resolve(result);
-              }
+              },
             )
             .end(buffer);
         });
@@ -50,36 +54,39 @@ export async function POST(req) {
       } catch (err) {
         return NextResponse.json(
           { error: "Media upload failed." },
-          { status: 500 }
+          { status: 500 },
         );
       }
 
       type = uploadResult?.resource_type === "video" ? "video" : "image";
     }
+    console.log("Cloudinary Upload Success");
 
     const newPost = await Post.create({
       user: userId,
-      content: content,
+      content,
       url: uploadResult?.secure_url || null,
-      type: type,
+      type,
     });
 
-    // console.log(newPost);
+    console.log("Post Created:", newPost._id);
 
     user.posts.push(newPost._id);
+
+    console.log("Saving User");
+
     await user.save();
+
+    console.log("User Saved");
 
     return NextResponse.json(
       {
         message: "Post created successfully",
       },
-      { status: 201 }
+      { status: 201 },
     );
   } catch (error) {
     console.log("POST /sendpost error:", error);
-    return NextResponse.json(
-      { error: "Something went wrong." },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: error?.message }, { status: 500 });
   }
 }
